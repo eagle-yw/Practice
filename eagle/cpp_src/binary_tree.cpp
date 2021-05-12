d64ca170e495c473d3251a4e480eeeab43357b5f
eagle <eagle84@aliyun.com>
2021-05-12 21:23:18 +0800

    Practice 1. binary tree update by eagle

--- eagle/cpp_src/binary_tree.cpp
+++ eagle/cpp_src/binary_tree.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <functional>
 
-
+//前序遍历
 template <typename T>
-void preorder_traversal(std::vector<T> &binary_tree, int curr_index, std::function<void(T curr)> func){
+void preOrderTraversal(std::vector<T> &binary_tree, int curr_index, std::function<void(T curr)> func){
     if(binary_tree.size() == 0 || func == nullptr){
         return;
     }
@@ -13,24 +13,73 @@ void preorder_traversal(std::vector<T> &binary_tree, int curr_index, std::functi
     
     int l_son = curr_index * 2 + 1;
     if(l_son < binary_tree.size()){
-        preorder_traversal(binary_tree, l_son, func);
+        preOrderTraversal(binary_tree, l_son, func);
     }
 
     int r_son = curr_index * 2 + 2;
     if(r_son < binary_tree.size()){
-        preorder_traversal(binary_tree, r_son, func);
+        preOrderTraversal(binary_tree, r_son, func);
     }
 }
+
+//中序遍历
+template <typename T>
+void inOrderTraversal(std::vector<T> &binary_tree, int curr_index, std::function<void(T curr)> func){
+    if(binary_tree.size() == 0 || func == nullptr){
+        return;
+    }
+    
+    int l_son = curr_index * 2 + 1;
+    if(l_son < binary_tree.size()){
+        inOrderTraversal(binary_tree, l_son, func);
+    }
+
+    func(binary_tree[curr_index]);
+
+    int r_son = curr_index * 2 + 2;
+    if(r_son < binary_tree.size()){
+        inOrderTraversal(binary_tree, r_son, func);
+    }
+}
+
+//后序遍历
+template <typename T>
+void postOrderTraversal(std::vector<T> &binary_tree, int curr_index, std::function<void(T curr)> func){
+    if(binary_tree.size() == 0 || func == nullptr){
+        return;
+    }
+    
+    int l_son = curr_index * 2 + 1;
+    if(l_son < binary_tree.size()){
+        postOrderTraversal(binary_tree, l_son, func);
+    }
+
+    int r_son = curr_index * 2 + 2;
+    if(r_son < binary_tree.size()){
+        postOrderTraversal(binary_tree, r_son, func);
+    }
+    
+    func(binary_tree[curr_index]);
+}
+
 
 
 int main(int argc, char* argv[]){
 
-    std::vector<int> binary_tree = { 3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6 }; 
+    std::vector<int> binary_tree = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
 
-    preorder_traversal(binary_tree, 0, (std::function<void(int)>)[](int curr){
+    std::function<void(int curr)> print = [](int curr){
         std::cout << curr << " ";
-    });
-
+    };
+    
+    preOrderTraversal(binary_tree, 0, print);
     std::cout << std::endl;
+    
+    inOrderTraversal(binary_tree, 0, print);
+    std::cout << std::endl;
+
+    postOrderTraversal(binary_tree, 0, print);
+    std::cout << std::endl;
+    
     return 0;
 }
