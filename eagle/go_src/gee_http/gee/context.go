a73bce012e07e4396f68e52620175c2ac4d293dc
eagle <eagle84@aliyun.com>
2021-05-26 21:22:06 +0800

    Practice 1. gee-web day3 trie update by eagle

--- eagle/go_src/gee_http/gee/context.go
+++ eagle/go_src/gee_http/gee/context.go
@@ -14,6 +14,8 @@ type Context struct {
 
 	Path string
 	Method string
+	Params map[string]string
+
 	StatusCode int
 }
 
@@ -67,4 +69,9 @@ func (c *Context)HTML(code int, html string){
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
 	c.Writer.Write([]byte(html))
+}
+
+func (c *Context)Param(key string) string{
+	value, _ := c.Params[key]
+	return value
 }
---
+++ eagle/go_src/gee_http/gee/gee_test.go
@@ -0,0 +1,24 @@
+package gee
+
+import (
+	"reflect"
+	"testing"
+)
+
+func newTestRouter() *router {
+	r := newRouter()
+	r.addRoute("GET", "/", nil)
+	r.addRoute("GET", "/hello/:name", nil)
+	r.addRoute("GET", "/assets/*filepath", nil)
+	return r
+}
+
+func TestParsePattern(t *testing.T) {
+	ok := reflect.DeepEqual(parsePattern("/p/:name"), []string{"p", ":name"})
+	if !ok {
+		t.Fatal("test parsePattern failed")
+	}
+}
+
+
+
