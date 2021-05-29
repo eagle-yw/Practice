e56c2843938a15e4004d0841136a820bb0b8fb30
eagle <eagle84@aliyun.com>
2021-05-29 18:02:07 +0800

    Practice 1. gee-web template update by eagle

--- eagle/go_src/gee_http/gee/gee.go
+++ eagle/go_src/gee_http/gee/gee.go
@@ -1,24 +1,62 @@
 package gee
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strings"
 )
 
 type HandlerFunc func(c *Context)
+
+
+
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup
+	htmlTemplates *template.Template
+	funcMap template.FuncMap
+}
+
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (engine *Engine)Run(addr string) (err error){
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine)ServeHTTP(w http.ResponseWriter, r *http.Request){
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(r.URL.Path, group.prefix){
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(w, r)
+	c.handlers = middlewares
+	c.engine = engine
+	engine.router.handle(c)
+}
+
+func (engine *Engine)SetFuncMap(funcMap template.FuncMap){
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine)LoadHTMLGlob(pattern string){
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
 
 type RouterGroup struct {
 	prefix 	string
 	middlewares []HandlerFunc
 	parent	*RouterGroup
 	engine *Engine
-}
-
-type Engine struct {
-	*RouterGroup
-	router *router
-	groups []*RouterGroup
 }
 
 func (group *RouterGroup)Group(prefix string) *RouterGroup{
@@ -50,26 +88,20 @@ func (group *RouterGroup)Use(middlewares... HandlerFunc){
 	group.middlewares = append(group.middlewares, middlewares...)
 }
 
-func New() *Engine {
-	engine := &Engine{router: newRouter()}
-	engine.RouterGroup = &RouterGroup{engine: engine}
-	engine.groups = []*RouterGroup{engine.RouterGroup}
-	return engine
-}
-
-func (engine *Engine)Run(addr string) (err error){
-	return http.ListenAndServe(addr, engine)
-}
-
-func (engine *Engine)ServeHTTP(w http.ResponseWriter, r *http.Request){
-	var middlewares []HandlerFunc
-	for _, group := range engine.groups {
-		if strings.HasPrefix(r.URL.Path, group.prefix){
-			middlewares = append(middlewares, group.middlewares...)
+func (group *RouterGroup)createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc{
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
 		}
+		fileServer.ServeHTTP(c.Writer, c.Req)
 	}
-	c := newContext(w, r)
-	c.handlers = middlewares
-	engine.router.handle(c)
 }
 
+func (group *RouterGroup)Static(relativePath string, root string){
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
--- eagle/go_src/gee_http/main.go
+++ eagle/go_src/gee_http/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"gee"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -15,19 +17,48 @@ func onlyForV2() gee.HandlerFunc {
 	}
 }
 
+type student struct {
+	Name string
+	Age int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
 func main(){
 	r := gee.New()
 	r.Use(gee.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("/home/eagle/Test/blog/templates/*")
 
-	r.GET("/index", func(c *gee.Context){
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Static("/assets", "/home/eagle/Test/blog/static")
+
+	stu1 := &student{Name: "wy", Age: 25}
+	stu2 := &student{Name: "eagle", Age: 25}
+
+
+	r.GET("/", func(c *gee.Context){
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gee.Context){
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title": "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+	r.GET("/date", func(c *gee.Context){
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now": time.Date(2021, 5, 29, 0,0,0,0,time.UTC),
+		})
 	})
 
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *gee.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello World!</h1>")
-		})
 		v1.GET("/hello", func(c *gee.Context){
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 		})
@@ -40,7 +71,7 @@ func main(){
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
 
-		v2.GET("/assets/*filepath", func(c *gee.Context) {
+		v2.GET("/assets/*filepath", func(c *gee.Context){
 			c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 		})
 
